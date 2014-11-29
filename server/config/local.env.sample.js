04e76ad9e8e6c3d382f58bbfb609c27a53f459b5
Tom Claasens <thomas.claasens@outlook.com>
2014-11-29 10:27:55 +0200

    Initial UI framework added.

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -6,8 +6,9 @@
 // You will need to set these on the server you deploy to.
 
 module.exports = {
-  DOMAIN: 'http://localhost:9000',
-  SESSION_SECRET: "shop-secret",
+  DOMAIN:           'http://localhost:9000',
+  SESSION_SECRET:   'shop-secret',
+
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
 };
